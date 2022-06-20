78a2c4f8655afd787631d033a7c6b05a69ff72b6
haripreetam <56575382+haripreetam@users.noreply.github.com>
2022-06-20 20:40:52 +0530

    Update GVHandcontrol.py

--- GVHandcontrol.py
+++ GVHandcontrol.py
@@ -1,69 +1,56 @@
-#@title
-import cv2
-import time
-import numpy as np
-import HandTrackModuleForG as htg
-import math
+import cv2 
+import mediapipe as mp
+from math import hypot
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-#developed ny AndrewMiras which helps to change the volume of pc 
-################################
-wCam, hCam = 640, 480
-################################
-cap = cv2.VideoCapture(1)
-cap.set(3, wCam)
-cap.set(4, hCam)
-pTime = 0
-detector = htg.handDetector(detectionCon=0.7)
+import numpy as np 
+
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands 
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+
 devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
-# volume.GetMute()
-# volume.GetMasterVolumeLevel()
-volRange = volume.GetVolumeRange()
-minVol = volRange[0]
-maxVol = volRange[1]
-vol = 0
-volBar = 400
-volPer = 0
+
+volMin,volMax = volume.GetVolumeRange()[:2]
+
 while True:
-    success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img, draw=False)
-    if len(lmList) != 0:
-        # print(lmList[4], lmList[8])
-        # will print the values of tip of thumb and index
-        x1, y1 = lmList[4][1], lmList[4][2]
-        x2, y2 = lmList[8][1], lmList[8][2]
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2 #this is giving the center of the two tips of fingers
-        cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-        # till here the code will print the circles on the given coordiantes
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED) #drawing circle at the given cx cy
-        length = math.hypot(x2 - x1, y2 - y1)   #using the hypotenous function
-        #which is given by greater value if the tips are far away from each other
-        #and will give smaller value if the tips are closer
-        # print(length)
-        # Hand range 50 - 300
-        # Volume Range -65 - 0
-        vol = np.interp(length, [50, 300], [minVol, maxVol])
-        volBar = np.interp(length, [50, 300], [400, 150])
-        volPer = np.interp(length, [50, 300], [0, 100])
-        print(int(length), vol)
-        #
+    success,img = cap.read()
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    lmList = []
+    if results.multi_hand_landmarks:
+        for handlandmark in results.multi_hand_landmarks:
+            for id,lm in enumerate(handlandmark.landmark):
+                h,w,_ = img.shape
+                cx,cy = int(lm.x*w),int(lm.y*h)
+                lmList.append([id,cx,cy]) 
+            mpDraw.draw_landmarks(img,handlandmark,mpHands.HAND_CONNECTIONS)
+    
+    if lmList != []:
+        x1,y1 = lmList[4][1],lmList[4][2]
+        x2,y2 = lmList[8][1],lmList[8][2]
+
+        cv2.circle(img,(x1,y1),4,(255,0,0),cv2.FILLED)
+        cv2.circle(img,(x2,y2),4,(255,0,0),cv2.FILLED)
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,0),3)
+
+        length = hypot(x2-x1,y2-y1)
+
+        vol = np.interp(length,[15,220],[volMin,volMax])
+        print(vol,length)
         volume.SetMasterVolumeLevel(vol, None)
-        if length &lt; 50:
-            cv2.circle(img, (cx, cy), 15, (0, 255, 0), cv2.FILLED)
-    cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 0), 3)
-    cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
-    cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
-                1, (255, 0, 0), 3)
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,1, (255, 0, 0), 3)
-    cv2.imshow("Img", img)
-    cv2.waitKey(1)
+
+        # Hand range 15 - 220
+        # Volume range -63.5 - 0.0
+        
+    cv2.imshow('Image',img)
+    if cv2.waitKey(1) & 0xff==ord('q'):
+        break
